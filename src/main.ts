d508b8dbb9ba1bf3b8606c460766a7954c428849
Fábio Maia <fabio.maia@itsector.pt>
2020-12-13 00:25:03 +0000

    Refined cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/main/app.module';
 import { setupSwagger } from './swagger';
 
 const port = process.env.PORT || 5000;
+const enableCors = process.env.CORS === 'true' || false;
 
 async function bootstrap() {
   const app = await NestFactory.create(
@@ -12,14 +13,11 @@ async function bootstrap() {
   }*/,
   );
   setupSwagger(app);
-  app.use((req, res, next) => {
-    console.log('New request', req.url);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  // app.enableCors();
+
+  if (enableCors) {
+    app.enableCors();
+  }
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);

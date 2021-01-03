a4affe11320f76c3a1536eec94d9dc4f21a0814b
Fábio Maia <fabio.maia@itsector.pt>
2021-01-03 01:53:40 +0000

    Fixed models

--- src/modules/quizzes/quiz.types.ts
+++ src/modules/quizzes/quiz.types.ts
@@ -1,11 +1,14 @@
+import { ApiModelProperty } from '@nestjs/swagger';
 import { QuizQuestion } from '../quizzesQuestions';
 import { QuizQuestionAnswer } from '../quizzesQuestionsAnswers';
 import { Quiz } from './quiz.entity';
 
-export interface QuizQuestionsData extends QuizQuestion {
+export class QuizQuestionsData extends QuizQuestion {
+  @ApiModelProperty({ type: QuizQuestionAnswer, isArray: true })
   answers: QuizQuestionAnswer[];
 }
 
 export class QuizData extends Quiz {
+  @ApiModelProperty({ type: QuizQuestionsData, isArray: true })
   questions: QuizQuestionsData[];
 }
